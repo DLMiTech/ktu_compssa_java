4c0e994871b1362cd40f27d6b33383468e0e66c7
DLMiTech <dlmitech.com@gmail.com>
2026-06-15 09:01:21 +0000

    Java operation

--- c_networking/networking_A/week3/src/JavaInput.java
+++ c_networking/networking_A/week3/src/JavaInput.java
@@ -6,10 +6,8 @@ public class JavaInput {
         //Create a scanner object
         Scanner input = new Scanner(System.in);
 
-
         int age;
         String name;
-        double salary;
 
         System.out.println("Enter your age: ");
         age = input.nextInt();
@@ -17,7 +15,7 @@ public class JavaInput {
         input.next();
         name = input.nextLine();
         System.out.println("Enter your salary: ");
-        salary = input.nextDouble();
+        double salary = input.nextDouble();
 
         System.out.printf("My name is %s and I am %d years old. With a salary of %.2f", name, age, salary);
 
---
+++ c_networking/networking_A/week3/src/JavaOperations.java
@@ -0,0 +1,22 @@
+public class JavaOperations {
+    public static void main(String[] args) {
+
+        //Arithmetic (+, -, *, /, %, ++, --)
+        int num1 = 27;
+        int num2 = 5;
+
+        System.out.println("10 + 3: "+(num1 + num2));
+        System.out.println("10 - 3: "+(num1 - num2));
+        System.out.println("10 * 3: "+(num1 * num2));
+        System.out.println("10 / 3: "+((double) num1 / num2));
+        System.out.println("10 %  3: "+(num1 * num2));
+        System.out.printf("%d remainder %d\n", num1/num2, num1%num2);
+
+        num1--;
+        System.out.println(num1);
+
+        //Assignment
+        //Comparison
+        //logical
+    }
+}
