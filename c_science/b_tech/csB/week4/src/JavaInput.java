aae9e56fbe2fd8244afa17dcc63d711324334695
DLMiTech <dlmitech.com@gmail.com>
2026-06-23 10:02:35 +0000

    week 4

---
+++ c_science/b_tech/csB/week4/src/JavaInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class JavaInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int age;
+        System.out.println("Enter your age: ");
+        age = input.nextInt();
+        System.out.println("Enter an amount: ");
+        double amount = input.nextDouble();
+        System.out.println("Enter your name: ");
+        input.nextLine();
+        String name = input.nextLine();
+        System.out.println("Enter your name: ");
+        String name2 = input.nextLine();
+
+        System.out.println(age);
+        System.out.println(amount);
+        System.out.println(name);
+        System.out.println(name2);
+    }
+}
